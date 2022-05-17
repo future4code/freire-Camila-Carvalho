c80649f66b22c2a45e7b70b998a84d2362759a35
Camila Gabriela Pinheiro de Carvalho <camilapinheiroc@gmail.com>
2022-05-17 17:12:33 -0300

    Resolução dos exercícios de condicionais

---
+++ condicionais-exercícios/exemplos.js
@@ -0,0 +1,46 @@
+// Exemplo 1
+
+//const condicao = true
+
+//if (condicao) {
+//    console.log("Entrei dentro do if pois minha condicao é true!")
+//}
+
+//  Exemplo 2
+
+const condicao = true
+
+if (condicao){
+    console.log("entrei no if, pois a condição é verdadeira")
+} else{
+    console.log("Entrei no else, pois minha condição é falsa")
+}
+
+
+// Exemplo 5
+
+let (paisDeOrigem) = prompt("Digite seu país de origem: ")
+switch (paisDeOrigem) {
+    case "Brasil":
+        console.log("Brasileiro")
+        break
+    case "Itália":
+        console.log("Italiano")
+        break
+    case "Finlandia":
+        console.log("Finlandes")
+        break
+    case "Inglaterra":
+        console.log("Inglês")
+        break
+    default:
+        
+
+
+
+
+
+
+    
+
+}
---
+++ condicionais-exercícios/exercicios.js
@@ -0,0 +1,49 @@
+//Exercícios 1
+
+// Crie uma função que:
+// Recebe 2 números (chamaremos de num1 e num2)
+// Compare esses números entre si:
+
+
+
+// function comparaNumero(num1, num2) {
+//     if (num1 === num2){
+//         console.log("Os dois números são iguais")
+//     }
+// }
+
+// const primeiroNumero = Number(prompt("Digite o primeiro número: "))
+// const segundoNumero = Number(prompt("Digite o segundo número: "))
+
+// exercício 2
+
+
+//Exercício 3
+
+// const checaNumeros = (num1, num2) => { //arrow function
+//      if (num1 > num2){
+//          console.log("O primeiro número é maior que o segundo número")
+//      } else if (num1 < num2) {
+//          console.log("O primeiro número é menor que o segundo número")
+//      } else {
+//          console.log("Os dois números são iguais")
+//      }
+
+// }
+
+// const primeiroNumero = Number(prompt("Digite o primeiro número"))
+// const segundoNumero = Number(prompt)("Digite o segundo número")
+
+// Exercício 4
+
+const pokemonInicial = prompt("Qual o seu pokemon inicial predileto?")
+
+
+
+
+     
+
+     
+     
+
+        
