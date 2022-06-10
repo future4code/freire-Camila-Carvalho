1efaaee3d1635bced1f7b04c6312b3b483ddbbf7
Camila Gabriela Pinheiro de Carvalho <camilapinheiroc@gmail.com>
2022-06-09 21:55:38 -0300

    exercicios de renderizacao de listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,35 +10,46 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+      nomeUsuario: 'paulinha',
+      fotoUsuario:'https://picsum.photos/50/50',
+      fotoPost: 'https://picsum.photos/200/150'
+    },
+    {
+      nomeUsuario:'aninha',
+      fotoUsuario:'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869761_960_720.jpg',
+      fotoPost:'https://cdn.pixabay.com/photo/2020/03/15/19/59/castle-4934812_960_720.jpg'
+    },
+    {
+      nomeUsuario:'bia',
+      fotoUsuario: 'https://cdn.pixabay.com/photo/2018/01/21/14/16/woman-3096664_960_720.jpg',
+      fotoPost:'https://cdn.pixabay.com/photo/2014/08/27/18/24/girl-429380_960_720.jpg'
+    }
+  ]
+  };
+
   render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
+    const listaPostsUsuarios = this.state.posts.map((usuario, index) => {
 
-        <Post
-          nomeUsuario={'aninha'}
-          fotoUsuario={'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869761_960_720.jpg'}
-          fotoPost={'https://cdn.pixabay.com/photo/2020/03/15/19/59/castle-4934812_960_720.jpg'}
-        />
+      return (
+        <div key={index}>
+          <Post
+          nomeUsuario = {usuario.nomeUsuario}
+          fotoUsuario = {usuario.fotoUsuario}
+          fotoPost = {usuario.fotoPost}
+          />
+        </div>);
+      
+      });
 
-        <Post
-          nomeUsuario={'bia'}
-          fotoUsuario={'https://cdn.pixabay.com/photo/2018/01/21/14/16/woman-3096664_960_720.jpg'}
-          fotoPost={'https://cdn.pixabay.com/photo/2014/08/27/18/24/girl-429380_960_720.jpg'}
-        />
-
-
-
-
-
-
-      </MainContainer>
-    );
-  }
+      return (
+      <MainContainer>{listaPostsUsuarios}</MainContainer>
+      );
+  };
+  
 }
+
 
 export default App;
