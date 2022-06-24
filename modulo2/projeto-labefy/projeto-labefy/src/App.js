e7a2e58f48ca9f05a359c3fcae07ea50209007a4
Camila Gabriela Pinheiro de Carvalho <camilapinheiroc@gmail.com>
2022-06-24 18:49:44 -0300

    labefy

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -1,17 +1,52 @@
-import './App.css';
+import React from 'react';
+import Reacct from 'react';
+import styled from 'styled-components';
+import AddMusica from './components/AddMusica';
 
-function App() {
-  return (
-    <div className="App">
-      <header >
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
-    </div>
-  );
+
+const fundoTela = styled.div`
+  background-color: #1a0c21;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  align-content: space-between;
+  
+`
+
+export default class App extends React.Component {
+
+  state = {
+    trocarTela: "mostrarPlaylists",
+  }
+
+  trocarTela = () => {
+    this.setState ({trocarTela: !this.state.trocarTela})
+  }
+
 }
 
-export default App;
+render() {
+
+  const trocandoTela = () => {
+    if (this.state.trocarTela === "mostrarPlaylists") {
+      return <Playlistst />
+    } if (this.state.trocarTela === "mostrarAddMusicas") 
+        return <AddMusica />
+    }
+  } 
+  return(
+    <Background>
+
+      <Header />
+      {trocandoTela()}
+
+    </Background> 
+  );
+}
+    
+};
+
+ 
+
---
+++ modulo2/projeto-labefy/projeto-labefy/src/components/AddMusica/AddMusica.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const inputs = styled.div`
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+
+`
